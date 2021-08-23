3aaaf7029249145a68b7347202f886e20da04900
ehsan <ehsanghochagh@gmail.com>
2021-08-23 15:07:48 +0430

    modified for visualize model pred randomly in plotly

--- app/app.py
+++ app/app.py
@@ -4,17 +4,28 @@ from joblib import load
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import uuid
+import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    test_np_input = np.array([[1], [2], [17]])
-    model = load('model.joblib')
-    preds = model.predict(test_np_input)
-    preds_as_str = str(preds)
-    return preds_as_str
+    request_type_str = request.method
+    if request_type_str == 'GET':
+        return render_template('index.html', href='static/base_pic.svg')
+    else:
+        text = request.form['text']
+        random_string = uuid.uuid4().hex
+        path = "static/" + random_string + ".svg"
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        for f in files:
+          print(f)
+        model = load('./model.joblib')
+        np_arr = floats_string_to_np_arr(text)
+        make_picture('./AgesAndHeights.pkl', model, np_arr, path)
+
+        return render_template('index.html', href=path[4:])
 
 
 
